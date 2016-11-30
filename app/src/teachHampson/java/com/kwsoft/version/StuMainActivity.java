489a51f421135f77082449fa6d27afd42183bbb5
rainsonking <rainsonking@163.com>
2016-11-30 09:11:33 +0800

    删除连按两次退出app操作

--- app/src/teachHampson/java/com/kwsoft/version/StuMainActivity.java
+++ app/src/teachHampson/java/com/kwsoft/version/StuMainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.kwsoft.kehuhua.adcustom.MessagAlertActivity;
 import com.kwsoft.kehuhua.adcustom.R;
@@ -220,6 +222,9 @@ public class StuMainActivity extends BaseActivity implements View.OnClickListene
     }
 
     private static long exitTime = 0;// 退出时间
+
+
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
