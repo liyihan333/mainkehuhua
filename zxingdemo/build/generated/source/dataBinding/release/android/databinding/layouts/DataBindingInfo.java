b3157ce27aaed6fea19c5c59e62538bf8280e9ce
Administrator <1224478011@qq.com>
2017-02-03 10:29:24 +0800

    12

---
+++ zxingdemo/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="14190d25-9878-48d5-a883-e80abf942054", modulePackage="cn.bingoogolapple.qrcode.zxingdemo", sdkRoot="E:\\android\\sdk",layoutInfoDir="D:\\kehuhua\\kehuhuanewmain\\zxingdemo\\build\\intermediates\\data-binding-info\\release",exportClassListTo="D:\\kehuhua\\kehuhuanewmain\\zxingdemo\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
